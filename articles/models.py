bc333ab31911bbd7bd2b78ee169e8df97ef16ef3
ubay1221 <ggarle030@gmail.com>
2024-12-26 00:04:19 +0500

    compleated

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -6,3 +7,6 @@ class Article(models.Model):
     short_content = models.TextField()
     long_content = models.TextField()
     author = models.CharField(max_length=100)
+
+    def get_detail_url(self):
+        return reverse('articles:detail', args=[self.pk])
